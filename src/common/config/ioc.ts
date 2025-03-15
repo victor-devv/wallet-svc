68b70c19b8152ae7f5b13533b584fff0295c551f
Victor Ikuomola <vheecthurikuomola@gmail.com>
2025-03-15 15:19:01 +0100

    feat: adds DI container

---
+++ src/common/config/ioc.ts
@@ -0,0 +1,13 @@
+import { Container } from 'inversify';
+
+// import controllers
+// import '../../server/controllers';
+
+const container = new Container();
+
+// bind services
+
+
+// bind repositories
+
+export default container;
